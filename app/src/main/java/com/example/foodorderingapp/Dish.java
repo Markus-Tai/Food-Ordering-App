dd967a288fa45b923d168d06db6be2a0af9645e2
MarkusT <markust0112@gmail.com>
2020-12-30 13:09:45 -0800

    added new classes

---
+++ app/src/main/java/com/example/foodorderingapp/Dish.java
@@ -0,0 +1,12 @@
+package com.example.foodorderingapp;
+
+public class Dish {
+
+    // Public properties
+    public String name;
+    public String description;
+    public Integer priceInCents;
+    public String imageResourceName;
+
+
+}
