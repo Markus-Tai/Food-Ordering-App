d34d727fb561897e22e7c1fd0f8e6ec269a4593c
akixuu <akixuu@users.noreply.github.com>
2021-01-10 20:50:54 -0800

    fixed bugs in class Dish

--- app/src/main/java/com/example/foodorderingapp/Dish.java
+++ app/src/main/java/com/example/foodorderingapp/Dish.java
@@ -1,6 +1,5 @@
 package com.example.foodorderingapp;
 
-<<<<<<< HEAD
 import androidx.annotation.NonNull;
 
 import com.thedeanda.lorem.Lorem;
@@ -17,13 +16,13 @@ public class Dish {
     private static Lorem lorem = LoremIpsum.getInstance();
     private int maxPriceInCents = 1999;
     private int minPriceInCents = 999;
+
     //constructor
     public Dish(String imageSource) {
         this.imageSource = imageSource;
 
         populateProperties();
     }
-
 
     //public methods
 
@@ -36,6 +35,7 @@ public class Dish {
                 ", imageSource='" + imageSource + '\'' +
                 '}';
     }
+
     public Boolean equals(Dish dish) {
         return (name.equals(dish.name) &&
                 description.equals(dish.description) &&
@@ -46,24 +46,13 @@ public class Dish {
     //private methods
     private void populateProperties() {
         //random self generating other properties...
-        String name = lorem.getTitle(1,4);
-        String description = lorem.getParagraphs(2,4);
+        String name = lorem.getTitle(1, 4);
+        String description = lorem.getParagraphs(2, 4);
         //normally you wont populate it like this
         //TODO maybe i can make random uber names lol that would be cool
         // using lorem.getName(), country, etc
         this.name = name;
         this.description = description;
-        this.priceInCents = (int)(Math.random() * (maxPriceInCents - minPriceInCents + 1) + minPriceInCents);
+        this.priceInCents = (int) (Math.random() * (maxPriceInCents - minPriceInCents + 1) + minPriceInCents);
     }
-=======
-public class Dish {
-
-    // Public properties
-    public String name;
-    public String description;
-    public Integer priceInCents;
-    public String imageResourceName;
-
-
->>>>>>> master
 }
